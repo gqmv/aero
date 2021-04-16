e4971806205e1deecf895716ba7ad17dd56c6806
gabri <gqmv@cin.ufpe.br>
2021-04-16 19:16:29 -0300

    - Adição de comentários nas configs

--- settings.py
+++ settings.py
@@ -1,23 +1,26 @@
 import os
 import pygame
 
-SCREEN_SIZE = (600, 750)
-MOVEMENT_SPEED = 5
+
+# Cores
+RED_COLOR = pygame.Color("Red")
+WHITE_COLOR = pygame.Color("White")
 BACKGROUND_COLOR = (120, 172, 255)
-WHITE_COLOR = (255, 255, 255)
-RED_COLOR = pygame.Color("RED")
 
+# Definições de janela
+SCREEN_SIZE = (600, 750)
 WINDOW_NAME = "Aero"
-
 BORDER_MARGIN_SIDES = 80
 BORDER_MARGIN_TOP = 60
 BORDER_WIDTH = 4
 
+# Definições de jogabilidade
 FPS = 60
+MOVEMENT_SPEED = 5
+GAS_SPAWN_CHANCE = 10  # (0-1000)
+GAS_PER_CAN = 0.1  # (0-1)
+GAS_CONS_PER_FRAME = 0.0005  # (0-1)
 
+# Assets
 GAS_ASSET = os.path.join("assets", "gas.png")
 PLANE_ASSET = os.path.join("assets", "plane.png")
-
-GAS_SPAWN_CHANCE = 10
-GAS_PER_CAN = 0.1
-GAS_CONS_PER_FRAME = 0.0005
