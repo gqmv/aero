7dd09c3ccaa0741c16534eab6178b5df2b5d6fef
gabri <gqmv@cin.ufpe.br>
2021-04-16 22:35:09 -0300

    Documentação do código

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ WINDOW = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption(WINDOW_NAME)
 
 
-def draw(all_sprites, gas_bar):
-    WINDOW.fill(BACKGROUND_COLOR)
-    all_sprites.draw(WINDOW)
+def draw(sprites: pygame.sprite.Group, gas_bar: Bar):
+    """
+    :param sprites: Group of stripes
+    :param gas_bar: The gas bar that will be drawn
 
+    This function renders all sprites, bars and borders.
+    """
+    WINDOW.fill(BACKGROUND_COLOR)
+    sprites.draw(WINDOW)
+
+    # Draws the border
     pygame.draw.rect(
         WINDOW,
         WHITE_COLOR,
         (
             BORDER_MARGIN_SIDES,
-            BORDER_MARGIN_TOP,
+            BORDER_MARGIN_TOP_BOTTOM,
             SCREEN_SIZE[0] - 2 * BORDER_MARGIN_SIDES,
-            SCREEN_SIZE[1] - 2 * BORDER_MARGIN_TOP,
+            SCREEN_SIZE[1] - 2 * BORDER_MARGIN_TOP_BOTTOM,
         ),
         BORDER_WIDTH,
     )
@@ -29,13 +36,30 @@ def draw(all_sprites, gas_bar):
     gas_bar.draw(WINDOW)
 
 
-def check_collision(sprite, group):
+def check_collision(sprite: pygame.sprite.Sprite, group: pygame.sprite.Group):
+    """
+    :param sprite: Sprite that will be used for collision checking
+    :param group: The group of sprites that will be checked
+    :return: bool
+
+    This function checks if any sprites from group collide with sprite. If so, returns True. Otherwise, returns False.
+    """
     if len(pygame.sprite.spritecollide(sprite, group, dokill=True)):
         return True
     return False
 
 
-def generate_gas(sprites, gas_cans):
+def generate_gas(sprites: pygame.sprite.Group, gas_cans: pygame.sprite.Group):
+    """
+    :param sprites: The sprite group that contains all sprites
+    :param gas_cans: The sprite group that contains all gas cans
+
+    This function generates a gas can if a random integer between 1 and 1000 is smaller than the GAS_SPAWN_CHANCE
+    constant, defined at settings.py.
+    The gas can has the following coordinates:
+    x: A random integer within the bordered area.
+    y: The top of the bordered area.
+    """
     if randint(1, 1000) < GAS_SPAWN_CHANCE:
         Object(
             GAS_ASSET,
@@ -43,21 +67,29 @@ def generate_gas(sprites, gas_cans):
                 BORDER_MARGIN_SIDES + BORDER_WIDTH,
                 SCREEN_SIZE[0] - BORDER_WIDTH - BORDER_MARGIN_SIDES,
             ),
-            BORDER_MARGIN_TOP + BORDER_WIDTH,
+            BORDER_MARGIN_TOP_BOTTOM + BORDER_WIDTH,
             sprites,
             gas_cans,
         )
 
 
-def move_gas(gas_cans):
-    for can in gas_cans:
-        can.y += 5
+def move_gas(gas_cans: pygame.sprite.Group):
+    """
+    :param gas_cans: The sprite group that contains all sprites
 
-        if can.y >= SCREEN_SIZE[1] - BORDER_MARGIN_TOP - BORDER_WIDTH - can.rect.height:
+    This function moves all gas cans (stored at gas_cans) SCROLL_SPEED pixels down.
+    """
+    for can in gas_cans:
+        can.y += SCROLL_SPEED
+
+        if can.y >= SCREEN_SIZE[1] - BORDER_MARGIN_TOP_BOTTOM - BORDER_WIDTH - can.rect.height:
             can.kill()
 
 
 def game_loop():
+    """
+    This function is the main game loop.
+    """
     loop = True
     clock = pygame.time.Clock()
 
@@ -86,7 +118,7 @@ def game_loop():
 
         mouse = pygame.mouse.get_pos()
 
-        plane.movement(mouse)
+        plane.handle_movement(mouse)
 
         generate_gas(sprites, gas_cans)
         move_gas(gas_cans)
--- objects.py
+++ objects.py
@@ -3,11 +3,20 @@ from settings import *
 
 
 class Bar(pygame.rect.Rect):
-    def __init__(self, x, y, width, height, default_percentage):
+    def __init__(
+        self, x: int, y: int, width: int, height: int, default_percentage: int
+    ):
         super().__init__(x, y, width, height)
         self.percentage = default_percentage
 
-    def draw(self, surface):
+    def draw(self, surface: pygame.Surface):
+        """
+        :param surface: The surface to which the bar will be drawn.
+
+        This function draws the bar's outline and it's current percentage.
+        """
+
+        # Fills the bar according to self.percentage
         pygame.draw.rect(
             surface,
             color=RED_COLOR,
@@ -18,12 +27,13 @@ class Bar(pygame.rect.Rect):
                 self.height * self.percentage,
             ),
         )
+
+        # Draws the outline
         pygame.draw.rect(surface, rect=self, color=WHITE_COLOR, width=4)
 
 
 class Object(pygame.sprite.Sprite):
-    # Classe geral para todo objeto do jogo
-    def __init__(self, image, x, y, *groups):
+    def __init__(self, image: str, x: int, y: int, *groups: pygame.sprite.Group):
         super().__init__(*groups)
         self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
@@ -32,18 +42,37 @@ class Object(pygame.sprite.Sprite):
 
     @property
     def x(self):
+        """
+        :return: The current x coordinate of self
+
+        This property returns the x position of the Object
+        """
         return self.rect.x
 
     @x.setter
-    def x(self, value):
+    def x(self, value: int):
+        """
+        :param value: The x coordinate that will set
+
+        This property sets the Object's x coordinate
+        """
         self.rect.x = value
 
     @property
     def y(self):
+        """
+        :return: The current y coordinate of self
+
+        This property returns the y position of the Object
+        """
         return self.rect.y
 
     @y.setter
-    def y(self, value):
+    def y(self, value: int):
+        """
+        :param value: The y coordinate that will set
+        :return: This property sets the Object's y coordinate
+        """
         self.rect.y = value
 
 
@@ -55,27 +84,49 @@ class Plane(Object):
         self.img_default = pygame.image.load(img)
         self.gas = 1
 
-    def movement(self, mouse):
+    def handle_movement(self, mouse: tuple):
+        """
+        :param mouse: The mouse position
+
+        This function moves the plane depending on the mice position.
+        """
         mouse_x = mouse[0]
         mouse_y = mouse[1]
 
         if (
-            mouse_x > self.x + self.rect.width
-            and self.x <= SCREEN_SIZE[0] - BORDER_MARGIN_SIDES - self.rect.width
+            mouse_x
+            > self.x
+            + self.rect.width  # Checks if the mouse is to the right of the plane
+            and self.x
+            <= SCREEN_SIZE[0]
+            - BORDER_MARGIN_SIDES
+            - self.rect.width  # Checks if the plane isn't at the border
         ):
-            self.image = self.img_right
+            self.image = (
+                self.img_right
+            )  # As the plane is moving right, set's the asset to self.img_right
             self.x += MOVEMENT_SPEED
 
-        elif mouse_x < self.x and self.x >= BORDER_MARGIN_SIDES:
-            self.image = self.img_left
+        elif (
+            mouse_x < self.x and self.x >= BORDER_MARGIN_SIDES
+        ):  # Checks if the mouse is to the left of the plane and within the border
+            self.image = (
+                self.img_left
+            )  # As the plane is moving left, set's the asset to self.img_left
             self.x -= MOVEMENT_SPEED
 
         if (
-            mouse_y > self.y + self.rect.height
+            mouse_y
+            > self.y + self.rect.height  # Checks if the mouse is under the plane
             and self.y
-            <= SCREEN_SIZE[1] - BORDER_MARGIN_TOP - BORDER_WIDTH - self.rect.height
-        ):
+            <= SCREEN_SIZE[1]
+            - BORDER_MARGIN_TOP_BOTTOM
+            - BORDER_WIDTH
+            - self.rect.height
+        ):  # Checks if the plane is at the border
             self.y += MOVEMENT_SPEED
 
-        elif mouse_y < self.y and self.y >= BORDER_MARGIN_TOP + BORDER_WIDTH:
+        elif (
+            mouse_y < self.y and self.y >= BORDER_MARGIN_TOP_BOTTOM + BORDER_WIDTH
+        ):  # Checks if the mouse is above the plane and within the border
             self.y -= MOVEMENT_SPEED
